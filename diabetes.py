2055c8b2b855f72cbd1e38fee6f633fc8cdd5a6a
beebongbenh <thangnh.info@gmail.com>
2025-07-11 15:02:42 +0700

    compare svm, lr, rf

--- diabetes.py
+++ diabetes.py
@@ -1,9 +1,10 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from ydata_profiling import ProfileReport
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
 df = pd.read_csv('data/diabetes.csv')
@@ -23,11 +24,21 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.fit_transform(X_test)
 
 # Train model
-model = SVC()
-model.fit(X_train, y_train)
+model_svm = SVC()
+model_svm.fit(X_train, y_train)
+y_pred_svm = model_svm.predict(X_test)
+metrics_svm = classification_report(y_test, y_pred_svm)
 
-y_pred = model.predict(X_test)
+model_lr = LogisticRegression()
+model_lr.fit(X_train, y_train)
+y_pred_lr = model_lr.predict(X_test)
+metrics_lr = classification_report(y_test, y_pred_lr)
 
-metrics = classification_report(y_test, y_pred)
+model_rf = RandomForestClassifier(n_estimators=100, random_state=42)
+model_rf.fit(X_train, y_train)
+y_pred_rf = model_rf.predict(X_test)
+metrics_rf = classification_report(y_test, y_pred_rf)
 
-print(metrics)
+print("SVM: ", metrics_svm)
+print("LR: ", metrics_lr)
+print("RF: ", metrics_rf)
